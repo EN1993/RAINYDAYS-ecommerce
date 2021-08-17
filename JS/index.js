84fb17b8b6705a7df02c4141031c07fc0444d385
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-08-17 17:33:00 +0200

    add

---
+++ JS/index.js
@@ -0,0 +1,15 @@
+const baseUrl ='https://sub.enlive.one/wp-json/wc/store/products';
+const productContainer = document.querySelector(".featured-products");
+
+
+async function getProducts(url) {
+  const response = await fetch(url);
+  const products = response.json();
+  products.forEach(function(product) {
+  productContainer.innerHTML +=`
+  <div class='product'> <h2> ${product.name} </h2> </div>
+  `
+ });
+    
+}
+getProducts(baseUrl);
