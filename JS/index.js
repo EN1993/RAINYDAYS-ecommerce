5ef6787dca3cc224d10b8cef9568d5e0e7cd1a91
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-08-19 14:47:04 +0200

    add

--- JS/index.js
+++ JS/index.js
@@ -7,7 +7,7 @@ async function getProducts(url) {
   const products = await response.json();
   products.forEach(function(product) {
   productContainer.innerHTML +=`
-  <a href="productSpecific.html?id=${product.id}}">
+  <a href="productSpecific.html?id=${product.id}">
   <div class='product'>
    <h2> ${product.name} </h2> 
    <div class='product-image'style="background-image:url('${product.images[0].src}')"> </div>
--- JS/productSpecific.js
+++ JS/productSpecific.js
@@ -3,7 +3,7 @@ const parms = new URLSearchParams(querystring);
 const id = parms.get("id");
 console.log(id);
 
-const url ="https://sub.enlive.one/wp-json/wc/store/products";
+const url ="https://sub.enlive.one/wp-json/wc/store/products/35";
 
 const specificContainer = document.querySelector(".Pspecific");
 
@@ -12,10 +12,11 @@ async function productSpecific() {
   const specific = await response.json();
   console.log(specific);
 
-  specificContainer.innerHTML+="";
+  
   specificContainer.innerHTML+=`<div>
                                   <h2>${specific.name}</h2>
-  
+                                  <div class='product-image'style="background-image:url('${specific.images[0].src}')"> </div>
+                                  <p>$ ${specific.prices.price}</p>
                                 </div>`
 }
 productSpecific();
